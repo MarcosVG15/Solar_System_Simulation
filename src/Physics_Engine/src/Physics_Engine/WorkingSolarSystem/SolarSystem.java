ed632ba182d6812873b325f6f24d4e6c57bf375d
AlexTru <o.trutenko@student.maastrichtuniversity.nl>
2025-06-03 00:59:22 +0200

    Update SolarSystem.java

--- src/Physics_Engine/src/Physics_Engine/WorkingSolarSystem/SolarSystem.java
+++ src/Physics_Engine/src/Physics_Engine/WorkingSolarSystem/SolarSystem.java
@@ -7,28 +7,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import Physics_Engine.src.Physics_Engine.WorkingSolarSystem.Interfaces.SolarSystemInterface;
+import Physics_Engine.src.Physics_Engine.WorkingSolarSystem.Interfaces.SpaceObject;
+import Physics_Engine.src.Physics_Engine.WorkingSolarSystem.Interfaces.vectorInterface;
 
-public class SolarSystem {
+public class SolarSystem implements SolarSystemInterface {
+    private final double EARTH_RADIUS = 6370 ;
 
-    private static SolarSystem instance;
 
-    private ArrayList<AstralObject> solarSystem;
+    private ArrayList<SpaceObject> solarSystem;
 
-    public static SolarSystem getInstance() {
-        if (instance == null) {
-            instance = new SolarSystem();
-        }
-        return instance;
-    }
 
-    private SolarSystem(){
+
+    public SolarSystem(){
 
         AstralObject sun = new AstralObject(
                 new Vector(0, 0, 0),
                 new Vector(0, 0, 0),
                 1.99E30
         );
-        sun.setName("SUN ");
+        sun.setName("SUN");
 
         AstralObject mercury = new AstralObject(
                 new Vector(13.9, -40.3, -4.57),
@@ -51,73 +49,137 @@ public class SolarSystem {
         );
         earth.setName("earth");
 
-        AstralObject moon = new AstralObject(
+        AstralObject moon =  new AstralObject(
                 new Vector(4.53, -28.6, 6.73E-2),
                 new Vector(-1.47E8, -2.95E7, 5.29E4),
                 7.35E22
         );
         moon.setName("moon");
 
-        AstralObject mars = new AstralObject(
+        AstralObject mars =  new AstralObject(
                 new Vector(-11.5, -18.7, -1.11E-1),
                 new Vector(-2.15E8, 1.27E8, 7.94E6),
                 6.42E23
         );
         mars.setName("mars");
 
-        AstralObject jupiter = new AstralObject(
+        AstralObject jupiter =  new AstralObject(
                 new Vector(-13.2, 12.9, 5.22E-2),
                 new Vector(5.54E7, 7.62E8, -4.40E6),
                 1.90E27
         );
         jupiter.setName("jupiter");
 
-        AstralObject saturn = new AstralObject(
+        AstralObject saturn =  new AstralObject(
                 new Vector(0.748, 9.55, -0.196),
                 new Vector(1.42E9, -1.91E8, -5.33E7),
                 5.68E26
         );
         saturn.setName("saturn");
 
-        AstralObject titan = new AstralObject(
+        AstralObject titan =  new AstralObject(
                 new Vector(5.95, 7.68, 0.254),
                 new Vector(1.42E9, -1.92E8, -5.28E7),
                 1.35E23
         );
         titan.setName("titan");
 
-        AstralObject uranus = new AstralObject(
+        AstralObject uranus =  new AstralObject(
                 new Vector(-5.72, 3.45, 8.70E-2),
                 new Vector(1.62E9, 2.43E9, -1.19E7),
                 8.68E25
         );
         uranus.setName("uranus");
 
-        AstralObject neptune = new AstralObject(
+        AstralObject neptune =  new AstralObject(
                 new Vector(0.0287, 5.47, -0.113),
                 new Vector(4.47E9, -5.31E7, -1.02E8),
                 1.02E26
         );
         neptune.setName("neptune");
 
+
+        //--------------------------------------------------------------------------------------------------------------
+
+
+        // Compute best coordinate to set the probe
+
+        double[] BestCoordinates = getBestInitialPositionCoordinates(titan.getPositionVector().getVector() , earth.getPositionVector().getVector());
+
+        // SpaceObject probe = new ProbeObject(
+        //         new Vector(  51.147313,-31.207901,-13.928841),
+        //         new Vector(BestCoordinates[0] , BestCoordinates[1] , BestCoordinates[2])
+
+        // );
+        // probe.setName("PROBE");
+
+
+        // SpaceObject SpaceShip = new SpaceShip(
+        //         new Vector(  0, 0,0),
+        //         new Vector(BestCoordinates[0]+1 , BestCoordinates[1] +1, BestCoordinates[2]+1 )
+
+        // );
+        // SpaceShip.setName("SpaceShip");
+
         solarSystem = new ArrayList<>();
 
-        solarSystem.add( sun);
-        solarSystem.add( mercury);
-        solarSystem.add( venus);
-        solarSystem.add( earth);
-        solarSystem.add( moon);
-        solarSystem.add( mars);
-        solarSystem.add( jupiter);
-        solarSystem.add( saturn);
-        solarSystem.add( titan);
-        solarSystem.add( uranus);
-        solarSystem.add(neptune);
+        solarSystem.add( (SpaceObject) sun);
+        solarSystem.add((SpaceObject) mercury);
+        solarSystem.add( (SpaceObject)venus);
+        solarSystem.add( (SpaceObject)earth);
+        solarSystem.add( (SpaceObject)moon);
+        solarSystem.add((SpaceObject) mars);
+        solarSystem.add( (SpaceObject)jupiter);
+        solarSystem.add((SpaceObject) saturn);
+        solarSystem.add( (SpaceObject)titan);
+        solarSystem.add((SpaceObject) uranus);
+        solarSystem.add( (SpaceObject)neptune);
+        // solarSystem.add( probe);
+        // solarSystem.add(SpaceShip);
 
     }
 
-    public ArrayList<AstralObject> getSolarSystem(){
+    public  ArrayList<SpaceObject> getSolarSystem(){
         return solarSystem;
+    }
+
+    /**
+     * To compute the best initial position for the rocket we compute the vector that is between the earth and titan
+     * after which we then scale it down to the radius of the earth by normalising and finally adding it to the vector
+     * of earth such that the position remains relative to the sun.
+     * @param Titan - coordinates of Titan - start coordinates
+     * @param Earth - coordinates of Earth - start coordinates
+     * @return
+     */
+    private double[] getBestInitialPositionCoordinates(double[] Titan , double[] Earth){
+
+        double [] InitialPosition = new double[3] ;
+        for( int i = 0 ; i< 3 ; i++){
+            InitialPosition[i] = Titan[i] - Earth[i] ;
+
+        }
+
+        // We Normalise scale and add at the same time to save on computation
+        double Normal = getModulus(new Vector(InitialPosition[0] , InitialPosition[1], InitialPosition[2]) , new Vector(0,0,0));
+
+        for(int i = 0 ; i<3 ; i++ ){
+            InitialPosition[i] = (InitialPosition[i]/Normal) * EARTH_RADIUS + Earth[i];
+        }
+
+        return InitialPosition ;
+    }
+    public double getModulus (vectorInterface vectorProbe , vectorInterface vectorTitan){
+
+        double[] probeValues = vectorProbe.getVector();
+        double[] titanValues = vectorTitan.getVector();
+
+        double sum =0 ;
+
+        for(int i = 0 ; i<3 ; i++){
+            sum += Math.pow((probeValues[i]- titanValues[i]), 2);
+        }
+
+        return Math.sqrt(sum);
     }
 
 }
