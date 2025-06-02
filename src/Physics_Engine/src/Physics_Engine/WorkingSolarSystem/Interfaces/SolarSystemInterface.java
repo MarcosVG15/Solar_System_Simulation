4339997c81d70818cff9b536fbb34b4a203ffeb7
AlexTru <o.trutenko@student.maastrichtuniversity.nl>
2025-06-03 00:57:37 +0200

    Create SolarSystemInterface.java interface

---
+++ src/Physics_Engine/src/Physics_Engine/WorkingSolarSystem/Interfaces/SolarSystemInterface.java
@@ -0,0 +1,7 @@
+package Physics_Engine.src.Physics_Engine.WorkingSolarSystem.Interfaces;
+
+import java.util.ArrayList;
+
+public interface SolarSystemInterface {
+    public ArrayList<SpaceObject> getSolarSystem();
+}
