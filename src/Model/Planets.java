db4be99e00f29bffe4d71602df93236744422b0f
AlexTru <o.trutenko@student.maastrichtuniversity.nl>
2025-06-03 00:33:19 +0200

    Updated Planets.java for proper animation of solar system

--- src/Model/Planets.java
+++ src/Model/Planets.java
@@ -1,51 +1,60 @@
 package Model;
 
-import Physics_Engine.src.Physics_Engine.SolarSystem.AstralObject;
+import Physics_Engine.src.Physics_Engine.WorkingSolarSystem.AstralObject;
+import Physics_Engine.src.Physics_Engine.WorkingSolarSystem.Vector;
+import Physics_Engine.src.Physics_Engine.WorkingSolarSystem.Interfaces.SpaceObject;
 import javafx.scene.image.Image;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
 
 public class Planets extends Sphere {
-    private AstralObject object;
+    private SpaceObject object;
     private String texturePath;
     private double scaleFactorForSun = 1e-7; // it's used to calculate radius of the sun with an appropriate size for simulation
     private double scaleFactorForPlanets = 2e-7; //it's used to calculate radius(3 times of an actual size for better visualization)
-    private double mass;
     
-    public Planets(double x , double y , double z , double Vx, double Vy, double Vz , double Mass, String name, String texture) {
-        this.mass = Mass;
-        this.object = new AstralObject(x, y, z, Vx, Vy, Vz, Mass, name);
-        double radius = calculateRadius(object);
-        super.setRadius(radius);
+    public Planets(SpaceObject object, String texture) {
+        this.object = object;
         this.texturePath = texture;
-
-        PhongMaterial textures = new PhongMaterial();
-        textures.setDiffuseMap(new Image(texturePath));
-        this.setMaterial(textures);
-    } 
-
-    public void updatePosition(double scale){
-        double[] coords = object.getCoordinates();
-        double visualScale = scale * 10; 
-        this.setTranslateX(coords[0] * visualScale);
-        this.setTranslateY(coords[1] * visualScale);
-        this.setTranslateZ(coords[2] * visualScale);
+        initiateVisualRepresentation();
     }
 
-    public double calculateRadius(AstralObject obj){
-        if(obj.getName().toLowerCase().equals("sun")){
-            double radius = Math.cbrt(mass) * scaleFactorForSun;
+    private void initiateVisualRepresentation(){
+        super.setRadius(calculateRadius(object));
+        PhongMaterial material = new PhongMaterial();
+        try {
+        Image texture = new Image(getClass().getResource(texturePath).toExternalForm());
+;
+        if (texture.isError()) {
+            throw new IllegalArgumentException("Texture loading error");
+        }
+        material.setDiffuseMap(texture);
+        } catch(Exception e){
+            material.setDiffuseColor(javafx.scene.paint.Color.RED);
+            System.err.println("Failed to load texture: " + texturePath);
+        }
+        this.setMaterial(material);
+    }
+
+    public void updatePosition(){
+        Vector position = (Vector) object.getPositionVector();
+        //System.out.println("Position for " + getName() + ": " + position.getX()  + ", " + position.getY()  + ", " + position.getZ());
+        
+        this.setTranslateX(position.getX() * 1e-5);
+        this.setTranslateY(position.getY() * 1e-5);
+        this.setTranslateZ(position.getZ() * 1e-5);
+    }
+
+    public double calculateRadius(SpaceObject obj){
+        if(object.getName().equalsIgnoreCase("sun")){
+            double radius = Math.cbrt(object.getMass()) * scaleFactorForSun;
             return Math.min(Math.max(radius, 2.0), 50.0);
         }
-        double radius = Math.cbrt(mass) * scaleFactorForPlanets;
+        double radius = Math.cbrt(object.getMass()) * scaleFactorForPlanets;
         return Math.min(Math.max(radius, 2.0), 50.0);
     }
 
-    public double getMass(){
-        return mass;
-    }
-
-    public AstralObject getAstralObject() {
+    public SpaceObject getSpaceObjectOf() {
         return object;
     }
 
