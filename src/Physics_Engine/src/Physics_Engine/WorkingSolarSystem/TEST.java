ac33acfdd32bcdcc306ab66bed065027a194b0ca
AlexTru <o.trutenko@student.maastrichtuniversity.nl>
2025-06-03 00:59:45 +0200

    Update TEST.java

--- src/Physics_Engine/src/Physics_Engine/WorkingSolarSystem/TEST.java
+++ src/Physics_Engine/src/Physics_Engine/WorkingSolarSystem/TEST.java
@@ -1,29 +1,38 @@
 package Physics_Engine.src.Physics_Engine.WorkingSolarSystem;
 
+
+import Physics_Engine.src.Physics_Engine.WorkingSolarSystem.Interfaces.SpaceObject;
+
+
 import java.util.ArrayList;
 
 public class TEST {
     public static void main(String[] args){
 
-        SolarSystem S = SolarSystem.getInstance();
+        SolarSystem S = new SolarSystem();
+        ArrayList<SpaceObject> solarSystem = S.getSolarSystem();
 
-        ArrayList<AstralObject> solarSystem = S.getSolarSystem();
 
-       for(int t = 0 ; t<10 ; t++){
+        long startTime = System.nanoTime();
 
-           for (AstralObject a: solarSystem){
-               a.print();
+       for(int t = 0 ; t<525600 ; t++){
+
+
+           for(SpaceObject object : solarSystem){
+               object.print();
            }
+
            AccelerationFunction acceleration = new AccelerationFunction();
            VelocityFunction velocity = new VelocityFunction();
            RK4_ODESolver odeSolver = new RK4_ODESolver();
 
-           odeSolver.ComputeODE(t , S, acceleration ,velocity);
-
-
-
+           odeSolver.ComputeODE(8 , S, acceleration ,velocity);
 
        }
+       long endTime = System.nanoTime();
+        long durationInNs = endTime - startTime;
+        System.out.println("Execution time: " + (durationInNs / 1_000_000_000.0) + " s");
+
 
 
 
