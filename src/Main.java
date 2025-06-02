fbc9b9abda25e3d3b92b612c439deb2b4ace5aa7
AlexTru <o.trutenko@student.maastrichtuniversity.nl>
2025-06-03 01:09:11 +0200

    The resolution is changed

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,11 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import Controller.SceneBuilder;
 
-/** The {@code Main} class serves as an initializer of the JavaFX application. It uses {@link SceneBuilder}   
- * 
- * 
- */
-
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
         BorderPane root = SceneBuilder.buildScene(stage);
-        Scene scene = new Scene(root, 800, 600, true);
+        Scene scene = new Scene(root, 1500, 800, true);
         stage.setScene(scene);
         stage.setTitle("Titan Expedition");
         stage.show();
